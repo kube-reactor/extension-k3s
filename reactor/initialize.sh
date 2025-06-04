efa74657ce16b9076b9b1d24bafc03f751648240
Adrian Webb <adrian@webb.sh>
2025-06-03 22:47:05 -0400

    Reenabling the CoreDNS addon in the reactor initialize script.

--- reactor/initialize.sh
+++ reactor/initialize.sh
@@ -8,5 +8,5 @@
 export __k3s_extension_dir="${2}"
 
 if [ "${KUBERNETES_PROVIDER:-}" == "k3s" ]; then
-  export INSTALL_K3S_EXEC="server --docker --disable=coredns --disable=metrics-server --disable=traefik"
+  export INSTALL_K3S_EXEC="server --docker --disable=metrics-server --disable=traefik"
 fi
